908db71837a1ccfb29911bfa7ba06157e156b8a4
juperameza <juperameza@hotmail.com>
2022-03-21 21:08:09 -0600

    Finished the code

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello world, this is the root route");
+});
+
+app.listen(3000);
+app.get("/uno", (req, res) => {
+  res.send("Hello world, from route One");
+});
+
+app.get("/prueba", (req, res) => {
+  res.send(`<h1> Hola desde la prueba usando html </h1>`);
+});
