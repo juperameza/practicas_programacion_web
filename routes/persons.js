a66eb7a23b2075798939b97d4bcdb7219bea6bed
juperameza <juperameza@hotmail.com>
2022-05-24 08:09:54 -0500

    FInish

---
+++ routes/persons.js
@@ -0,0 +1,33 @@
+const express = require("express"); //inyectamos una dependencia de express
+const router = express.Router(); //declaramos las rutas
+const mongoose = require("../node_modules/mongoose"); //Inyectamos moongose
+let Person = require("../models/person");
+
+router.get("/persons", (req, res, next) => {
+  Person.find((err, persons) => {
+    if (err) return next(err);
+    res.render("persons.ejs", { persons: persons });
+  });
+}); //definimos la ruta de persona que devuelve un json si no hay error
+
+router.get("/person", (req, res) => {
+  res.render("person");
+});
+
+router.post("/addPerson", (req, res) => {
+  const myPerson = new Person({
+    nombre: req.body.nombre,
+    edad: req.body.edad,
+    tipoSangre: req.body.tipoSangre,
+    nss: req.body.nss,
+  });
+  myPerson.save((err, doc) => {
+    if (err) throw err;
+    else {
+      res.render("index.ejs");
+      console.log("User saved successfully!", doc);
+    }
+  });
+});
+
+module.exports = router;
